a66236312af166f83f6354d40ca79e3e9fec1322
Shruti Nair <55981201+Shru890@users.noreply.github.com>
2020-08-28 10:03:35 +0530

    added files

---
+++ app.js
@@ -0,0 +1,19 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/',(req, res)=> {
+// 	res.send("How are you from express")
+// })
+
+// app.listen(3000);
+
+const fs = require("fs");
+const fileName = "target.txt";
+fs.readFile(fileName, (err, data) => {
+	if(err) {
+		console.log(err);
+	}
+	console.log(data.toString());
+});
+
+console.log("Node js async programming.....?");
